63907d4ac6c8563dc470013529b9d2eaed59d7d3
袁晓宇 <3216320895@qq.com>
2020-10-13 17:40:13 +0800

    ff

--- src/app.js
+++ src/app.js
@@ -23,28 +23,3 @@ class Server {
 }
 
 module.exports = Server;
-// const http = require('http');
-// const chalk = require('chalk');
-// const path = require('path');
-// const conf = require('./config/defaultConfig');
-// const route = require('./helper/route');
-
-// class Server {
-//   constructor(config) {
-//     this.conf = Object.assign({}, conf, config);
-//   }
-
-//   start() {
-//     const server = http.createServer((req, res) => {
-//       const filePath = path.join(this.conf.root, req.url);
-//       route(req, res, filePath, this.conf);
-//     });
-
-//     server.listen(process.env.npm_package_config_zhangsan, this.conf.hostname, () => {
-//       const addr = `http://${this.conf.hostname}:${this.conf.port}`;
-//       console.info(`Server started at ${chalk.green(addr)}`);
-//     });
-//   }
-// }
-
-// module.exports = Server;
